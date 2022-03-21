b4f652eeafe8ae6e73d53ff3ec39b6903400db9d
swpr0714 <swpr0714@gmail.com>
2022-03-21 15:15:34 +0800

    upload 22/03/21

---
+++ CPP Adavance Program Design/float.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    float temperature = 1.000000000000000000000000;
+
+    cout << fixed << setprecision(3) << temperature << endl;
+}
+
+
+
